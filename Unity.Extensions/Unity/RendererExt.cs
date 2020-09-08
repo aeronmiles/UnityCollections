2628c5f0025dbb75847df6c74483121fea2b452f
aeronmiles <aeron.miles@diverseinteractive.com>
2020-09-08 10:38:35 +0100

    Add

--- Unity.Extensions/Unity/RendererExt.cs
+++ Unity.Extensions/Unity/RendererExt.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,23 +15,57 @@ public static class RendererExt
         return renderers;
     }
 
-    public static List<Renderer> SetActive(this List<Renderer> objs, bool active)
+    public static List<Renderer> SetActive(this List<Renderer> renderers, bool active)
     {
-        int l = objs.Count;
+        int l = renderers.Count;
         for (int i = 0; i < l; i++)
         {
-            objs[i].gameObject.SetActive(active);
+            renderers[i].gameObject.SetActive(active);
         }
-        return objs;
+        return renderers;
     }
 
-    public static Renderer[] SetActive(this Renderer[] objs, bool active)
+    public static Renderer[] SetActive(this Renderer[] renderers, bool active)
     {
-        int l = objs.Length;
+        int l = renderers.Length;
         for (int i = 0; i < l; i++)
         {
-            objs[i].gameObject.SetActive(active);
+            renderers[i].gameObject.SetActive(active);
         }
-        return objs;
+        return renderers;
+    }
+
+    public static Material[] SharedMaterials(this Renderer[] renderers)
+    {
+        int n = 0;
+        foreach (var r in renderers) 
+            n += r.sharedMaterials.Length;
+        
+        Material[] materials = new Material[n];
+        int i = 0;
+        foreach (var r in renderers)
+        {
+            var mats = r.sharedMaterials;
+            foreach (var m in mats) materials[i++] = m;
+        }
+
+        return materials;
+    }
+    
+    public static Material[] Materials(this Renderer[] renderers)
+    {
+        int n = 0;
+        foreach (var r in renderers) 
+            n += r.materials.Length;
+        
+        Material[] materials = new Material[n];
+        int i = 0;
+        foreach (var r in renderers)
+        {
+            var mats = r.materials;
+            foreach (var m in mats) materials[i++] = m;
+        }
+
+        return materials;
     }
 }
