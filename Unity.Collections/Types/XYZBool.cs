57d5c0a6f4fad127f9d3dba4f48c5bf2e84d066f
Aeron Miles <aeronmiles@users.noreply.github.com>
2020-11-12 16:19:59 +0000

    Update XYZBool.cs

--- Unity.Collections/Types/XYZBool.cs
+++ Unity.Collections/Types/XYZBool.cs
@@ -1,8 +1,14 @@
 [System.Serializable]
 public struct XYZBool
 {
-    public bool Any => X || Y || Z;
     public bool X;
     public bool Y;
     public bool Z;
+}
+
+public static class XYZBoolExt
+{
+    public static bool Any(this XYZBool xyzbool) => xyzbool.X || xyzbool.Y || xyzbool.Z;
+    public static bool None(this XYZBool xyzbool) => !xyzbool.X && !xyzbool.Y && !xyzbool.Z;
+    public static bool All(this XYZBool xyzbool) => xyzbool.X && xyzbool.Y && xyzbool.Z;
 }
