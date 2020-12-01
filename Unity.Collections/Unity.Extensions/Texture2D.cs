1f5ae6a1fd1d044d5e306fd47df2bca703758377
Aeron <aeron.miles@diverseinteractive.com>
2020-12-01 13:38:15 +0000

    Add Texture2D load from path

--- Unity.Collections/Unity.Extensions/Texture2D.cs
+++ Unity.Collections/Unity.Extensions/Texture2D.cs
@@ -4,20 +4,18 @@ public static class Texture2DExt
 {
     public static Texture2D LoadTexture(string FilePath)
     {
- 
         // Load a PNG or JPG file from disk to a Texture2D
         // Returns null if load fails
- 
         Texture2D Tex2D;
         byte[] FileData;
  
         if (File.Exists(FilePath))
         {
             FileData = File.ReadAllBytes(FilePath);
-            Tex2D = new Texture2D(2, 2);           // Create new "empty" texture
-            if (Tex2D.LoadImage(FileData))           // Load the imagedata into the texture (size is set automatically)
-                return Tex2D;                 // If data = readable -> return texture
+            Tex2D = new Texture2D(2, 2);
+            if (Tex2D.LoadImage(FileData))
+                return Tex2D;
         }
-        return null;                     // Return null if load failed
+        return null;
     }
 }
