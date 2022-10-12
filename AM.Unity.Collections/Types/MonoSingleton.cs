89b2dc547f6ced9072abf6189704c7ccf2585635
Aeron <aeronmiles@yahoo.co.uk>
2022-10-12 09:37:56 +0100

    merge latest chagnes

--- AM.Unity.Collections/Types/MonoSingleton.cs
+++ AM.Unity.Collections/Types/MonoSingleton.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using UnityEngine;
 
 public class MonoSingleton<T> : MonoBehaviour where T : Component
@@ -35,41 +34,3 @@ public class MonoSingleton<T> : MonoBehaviour where T : Component
     }
 
 }
-=======
-using UnityEngine;
-
-public class MonoSingleton<T> : MonoBehaviour where T : Component
-{
-    private static T _instance = null;
-
-    private static readonly object _lock = new();
-    public static T I
-    {
-        get
-        {
-            lock (_lock)
-            {
-                if (_instance != null) return _instance;
-                if (Application.isPlaying)
-                    _instance = new GameObject(typeof(T).Name).AddComponent<T>();
-                return _instance;
-            }
-        }
-    }
-
-    public virtual void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this as T;
-            DontDestroyOnLoad(this);
-        }
-        else
-        {
-            Debug.Log($"MonoSingleton<{typeof(T)}> Awake() :: Destroy", this);
-            DestroyImmediate(gameObject);
-        }
-    }
-
-}
->>>>>>> 9c31c7c3bd33c3d67b5c9a1a3d71d3be10f4427e
