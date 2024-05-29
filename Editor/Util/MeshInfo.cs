2d8d8a504eb13dd1fd780f1671e896c064a2c16a
Aeron Miles <aeron.miles@flipsidegroup.com>
2024-05-29 10:22:35 +0100

    Add MeshInfo editor script

---
+++ Editor/Util/MeshInfo.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MeshFilter))]
+public class MeshInfo : Editor
+{
+  // void OnSceneGUI()
+  // {
+  //     MeshFilter meshFilter = target as MeshFilter;
+  //     if (meshFilter != null && meshFilter.sharedMesh != null)
+  //     {
+  //         ShowVertexIndices(meshFilter);
+  //     }
+  // }
+
+  private void ShowVertexIndices(MeshFilter meshFilter)
+  {
+    Mesh mesh = meshFilter.sharedMesh;
+    Vector3[] vertices = mesh.vertices;
+
+    // Transform vertex position from local space to world space
+    Transform transform = meshFilter.transform;
+
+    // Set style for labels
+    GUIStyle labelStyle = new GUIStyle();
+    labelStyle.normal.textColor = Color.white;
+    labelStyle.fontSize = 10;
+
+    for (int i = 0; i < vertices.Length; i++)
+    {
+      Vector3 vertexWorldPosition = transform.TransformPoint(vertices[i]);
+      Handles.Label(vertexWorldPosition, $" {i}", labelStyle);
+    }
+  }
+}
