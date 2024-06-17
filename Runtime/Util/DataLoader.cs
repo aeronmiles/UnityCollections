7fb80ad26d12637c321761e2b4f2cd15f2f3c0cf
Aeron Miles <aeron.miles@flipsidegroup.com>
2024-06-17 13:33:46 +0100

    Cleanup

--- Runtime/Util/DataLoader.cs
+++ Runtime/Util/DataLoader.cs
@@ -5,108 +5,108 @@ using UnityEngine;
 
 public static class DataLoader
 {
-    private static Data m_data = new Data();
+  private static Data m_data = new Data();
 
-    private static string DataFilePath => Path.Combine(Application.persistentDataPath, "data.dat");
+  private static string DataFilePath => Path.Combine(Application.persistentDataPath, "data.dat");
 
-    public static void Load<T>(Action<T> onDataLoaded) where T : class, new()
+  public static void Load<T>(Action<T> onDataLoaded) where T : class, new()
+  {
+    Environment.SetEnvironmentVariable("MONO_REFLECTION_SERIALIZER", "yes"); // for ios
+    LoadData(onDataLoaded);
+  }
+
+  private static void LoadData<T>(Action<T> onDataLoaded) where T : class, new()
+  {
+    if (m_data.HasType<T>())
     {
-        Environment.SetEnvironmentVariable("MONO_REFLECTION_SERIALIZER", "yes"); // for ios
-        LoadData(onDataLoaded);
+      onDataLoaded?.Invoke(m_data.GetDataOfType<T>());
+      Save();
+      return;
     }
 
-    private static void LoadData<T>(Action<T> onDataLoaded) where T : class, new()
-    {
-        if (m_data.HasType<T>())
-        {
-            onDataLoaded?.Invoke(m_data.GetDataOfType<T>());
-            Save();
-            return;
-        }
+    DataUtil.Load(ref m_data, DataFilePath);
 
-        DataUtil.Load(ref m_data, DataFilePath);
+    if (m_data == null)
+      m_data = new Data();
 
-        if (m_data == null)
-            m_data = new Data();
+    if (!m_data.HasType<T>())
+      m_data.SetDataOfType(new T());
 
-        if (!m_data.HasType<T>())
-            m_data.SetDataOfType(new T());
+    onDataLoaded?.Invoke(m_data.GetDataOfType<T>());
+    Save();
+  }
 
-        onDataLoaded?.Invoke(m_data.GetDataOfType<T>());
-        Save();
-    }
+  public static void Save() => DataUtil.Save(m_data, DataFilePath);
 
-    public static void Save() => DataUtil.Save(m_data, DataFilePath);
+  public static void Save<T>(T data) where T : class, new()
+  {
+    m_data.SetDataOfType(data);
+    Save();
+  }
 
-    public static void Save<T>(T data) where T : class, new()
-    {
-        m_data.SetDataOfType(data);
-        Save();
-    }
-
-    public static void Reset<T>() where T : class, new()
-    {
-        Debug.LogWarning("DataSerializer -> reset() :: Resetting data!");
-        m_data.SetDataOfType(new T());
-        Save();
-    }
+  public static void Reset<T>() where T : class, new()
+  {
+    Debug.LogWarning("DataSerializer -> reset() :: Resetting data!");
+    m_data.SetDataOfType(new T());
+    Save();
+  }
 }
 
 [Serializable]
 public class Data
 {
-    public List<object> Classes = new List<object>();
+  public List<object> Classes = new List<object>();
 }
 
 public static class DataExt
 {
-    public static T GetDataOfType<T>(this Data data) where T : class, new()
+  public static T GetDataOfType<T>(this Data data) where T : class, new()
+  {
+    foreach (var d in data.Classes)
     {
-        foreach (var d in data.Classes)
-        {
-            if (d is T dt)
-                return dt;
-        }
-
-        return null;
+      if (d is T dt)
+        return dt;
     }
 
-    public static void SetDataOfType<T>(this Data data, T newData) where T : class, new()
-    {
-        int l = data.Classes.Count;
-        for (int i = 0; i < l; i++)
-        {
-            if (data.Classes[i] is T)
-            {
-                data.Classes[i] = newData;
-                return;
-            }
-        }
+    return null;
+  }
 
-        data.Classes.Add(newData);
+  public static void SetDataOfType<T>(this Data data, T newData) where T : class, new()
+  {
+    int l = data.Classes.Count;
+    for (int i = 0; i < l; i++)
+    {
+      if (data.Classes[i] is T)
+      {
+        data.Classes[i] = newData;
+        return;
+      }
     }
 
-    public static void RemoveDataOfType<T>(this Data data)
-    {
-        object toRemove = null;
-        foreach (var d in data.Classes)
-        {
-            if (d is T)
-            {
-                toRemove = d;
-                return;
-            }
-        }
+    data.Classes.Add(newData);
+  }
 
-        data.Classes.Remove(toRemove);
+  public static void RemoveDataOfType<T>(this Data data)
+  {
+    object toRemove = null;
+    foreach (var d in data.Classes)
+    {
+      if (d is T)
+      {
+        toRemove = d;
+        return;
+      }
     }
 
-    public static bool HasType<T>(this Data data) where T : class, new()
-    {
-        foreach (var d in data.Classes)
-            if (d is T)
-                return true;
+    data.Classes.Remove(toRemove);
+  }
 
-        return false;
-    }
+  public static bool HasType<T>(this Data data) where T : class, new()
+  {
+    foreach (var d in data.Classes)
+      if (d is T)
+        return true;
+
+    return false;
+  }
 }
--- Runtime/Util/DataUtil.cs
+++ Runtime/Util/DataUtil.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using Newtonsoft.Json;
-using Microsoft.SqlServer.Server;
 
 public static class DataUtil
 {
