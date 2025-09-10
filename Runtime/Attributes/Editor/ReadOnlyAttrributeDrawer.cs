8c38b561bc83e2b1cc6eab35ca0ff3fa259fe275
Aeron Miles <aeron.miles@flipsidegroup.com>
2025-09-10 20:35:00 +0100

    Add ReadOnly field attribute

---
+++ Runtime/Attributes/Editor/ReadOnlyAttrributeDrawer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// This class tells Unity how to draw any field marked with the [ReadOnly] attribute.
+/// </summary>
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyAttributeDrawer : PropertyDrawer
+{
+  // Override the OnGUI method to draw the property
+  public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+  {
+    // Save the previous GUI state
+    bool previousGUIState = GUI.enabled;
+
+    // Disable editing
+    GUI.enabled = false;
+
+    // Draw the property field as a disabled field
+    EditorGUI.PropertyField(position, property, label);
+
+    // Restore the previous GUI state
+    GUI.enabled = previousGUIState;
+  }
+}
